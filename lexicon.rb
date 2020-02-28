89dccd6649fcd3ceb0d08af13023a75b114f41cc
fatimarabab <fr.rabab@gmail.com>
2020-02-28 00:44:04 -0600

    Implemented Anagram algorithm and made changes in Lexicon class

--- lexicon.rb
+++ lexicon.rb
@@ -5,16 +5,30 @@ class Lexicon
     @array = []
     # A hash containing all the words as keys
     @hash = {}
+    # A hash containing sorted word as key and values as all corresponding words
+    # Example: {"act" => ["cat", "act"]}
+    @sorted_hash = Hash.new{|hsh,key| hsh[key] = [] }
 
     file = File.new('words.txt', 'r')
     while (line = file.gets)
       line.strip!.downcase!
       @array.push line
       @hash[line] = true
+      word = line
+      word.chars.sort.join
+      if is_sorted_key?(word)
+        @sorted_hash[@word] << line
+      else
+        @sorted_hash[@word] = []
+        @sorted_hash[@word] << line
+      end
     end
     file.close
   end
 
+  def is_sorted_key?(word)
+    return @sorted_hash.has_key?(word)
+  end
 
   # Returns true if the given word is in the lexicon
   def is_word?(word)
--- special_lexicon.rb
+++ special_lexicon.rb
@@ -1,17 +1,50 @@
 require './lexicon'
 
+class String
+  def sorted_letters
+    downcase.chars.sort.join
+  end
+end
+
 class SpecialLexicon < Lexicon
 
   # Generates an array of all the anagrams of the given word
+  # You can find the anagram for marsipobranchiata
+  # You do it in better than O(n) time, for n = size of the dictionary
+
+  # Approach is to hash words in the dictionary such that
+  # key stores sorted letters; value points to all anagrams in the dictionary
+  # Example of [key => value] pair => { "act" => ["cat", "act"] }
+  # Call is_word from Lexicon class and if true return values from hash
+
+  # @param {String} word
+  # @return {String[]}
   def get_anagrams(word)
     # FILL ME IN
-    return []
+    #returns empty array is empty word is entered
+    return [] if word.length < 1
+
+    # If word length is 1 i.e. word is a single letter
+    if word.length == 1
+      return word
+    end
+    
+    # Checks if the word exists in the dictionary i.e. it's a real word
+    # If true returns anagrams from sorted_hash
+    # Example: word = act sorted_hash["act"] => ["cat", "act"]
+    # else return empty array
+    if is_word?(word)
+      return @sorted_hash[word.chars.sort.join]
+    else 
+      return []
+    end
   end
 
 
   # Generates an array of all the words that have the given word as a prefix
   def get_prefixed_words(prefix)
     # FILL ME IN
+
     return []
   end
 
